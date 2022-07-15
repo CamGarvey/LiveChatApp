c44cf00b80a2c8a0fb49a4bd1696e3055a830b6a
CamGarvey <41130011+CamGarvey@users.noreply.github.com>
2022-07-16 08:05:57 +1200

    remmove ignored files

--- server/dist/graphql/schema/shared/fenum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FriendStatus = void 0;
-const nexus_1 = require("nexus");
-exports.FriendStatus = (0, nexus_1.enumType)({
-    name: 'FriendStatus',
-    members: ['FRIEND', 'REQUEST_SENT', 'REQUEST_RECEIVED', 'NOT_FRIEND'],
-});
-//# sourceMappingURL=fenum.js.map
