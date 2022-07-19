013669703e0b0e3309b1e08d11431bb87ec9a83a
CamGarvey <41130011+CamGarvey@users.noreply.github.com>
2022-07-19 15:20:48 +1200

    remove event folder

--- server/src/graphql/schema/event/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './message.interface';
-export * from './message.object';
-export * from './message.query';
-export * from './message.mutation';
-export * from './message.subscription';
